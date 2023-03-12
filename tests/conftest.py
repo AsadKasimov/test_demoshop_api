89eeba32eb751c306dead283c8de97ed6712ce18
GithubRedMouth <90645738+GithubRedMouth@users.noreply.github.com>
2023-03-12 06:22:04 +0300

    интеграция с selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,34 @@
+from utils.base_session import demoshop
+import os
 import pytest
 from selene.support.shared import browser
-from utils.base_session import demoshop
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
 
 
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 @pytest.fixture(scope='session')
 def register():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    login = os.getenv('SELENOID_LOGIN')
+    password = os.getenv('SELENOID_PASSWORD')
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options)
+    browser.config.driver = driver
+
     browser.config.base_url = "https://demowebshop.tricentis.com/"
     browser.config.window_width = 1920
     browser.config.window_height = 1080
